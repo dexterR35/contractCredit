84696327f30133676f5b053e0b84b22700aa79f7
dexterR35 <pusaw202@gmail.com>
2024-03-04 17:06:39 +0200

    add emailjs and pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import MyForm from "./components/ContractForm";
 import DisplayData from "./components/DisplayData";
-
 function App() {
   return (
     <>
       <MyForm />
       <DisplayData />
+
     </>
   );
 }
--- src/components/TextCredit.jsx
+++ src/components/TextCredit.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TextCredit = () => {
+const TextCredit = ({ textContent }) => {
   return (
     <div>
       <ul>
