519c7271f6e1354ccd3ed7bcc5babac906570acb
dexterR35 <pusaw202@gmail.com>
2025-12-02 15:31:20 +0200

    fix

--- src/components/TextObject.jsx
+++ src/components/TextObject.jsx
@@ -2,7 +2,7 @@ export const contractSections = [
     {
         title: "",
         items: [
-            "SOCIETATEA OBTINECREDIT.RO SRL, cu sediul social în Bucureşti, SECTOR 1, Str. Hrisovului Nr. 5A, CAMERA 1, Bloc. 2, Scara 1, Etaj 4, nr. telefon 0786.744.694, înregistrată în Registrul Comerţului sub nr. J40/11953/2023, și CIF 48406675, Cont RO85RNCB0082175670920001 deschis la Banca Comerciala Roaman S.A., reprezentată legal de către Dl. Hodor Florin, în calitate de consultant financiar."
+            "  SOCIETATEA NEXUM STRATEGIX CONSULT S.R.L SRL, cu sediul social în Bucureşti, SECTOR 1, Str. Hrisovului Nr. 5A, CAMERA nr. 1, Bloc. 2, Scara 1, Etaj 4, Ap. 38, Telefon 0786.744.694. Înregistrată în Registrul Comerţului sub nr. J2024041427000, și CIF 50888185, Cont RO92INGB0000999917118009 deschis la ING BANK, reprezentată legal de către Dl. Hodor Florin, în calitate de consultant financiar."
         ]
     },
     {
